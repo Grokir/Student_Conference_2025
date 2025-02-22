97c772ad6291ce92a4326de0608b8710a2c80698
Grokir <132129213+Grokir@users.noreply.github.com>
2025-02-22 20:42:27 +0300

    Update read.php
    Исправлены пути при чтении файла

--- sec_app/api/read.php
+++ sec_app/api/read.php
@@ -2,8 +2,8 @@
   <?php
     if (isset($_GET['file'])) {
       $file_name    = $_GET['file'];
-      $workdir_path = '/var/www/php-app/';
-      $location     = basename(realpath($workdir_path.$file_name));
+      $workdir_path = '/var/www/php-app/api/';
+      $location     = $workdir_path.basename(realpath($file_name));
       echo file_get_contents($location);
     }
   ?>
